66e81a46743dfe16c4fe2c4eaf9199de483f7100
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-02-15 13:53:40 +0530

    Add files via upload

---
+++ rds.tf
@@ -0,0 +1,36 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage      = 20
+  identifier = "book-rds"
+  db_subnet_group_name   = aws_db_subnet_group.sub-grp.id
+  engine                 = "mysql"
+  engine_version         = "8.0.32"
+  instance_class         = "db.t3.micro"
+  multi_az               = true
+  db_name                = "mydb"
+  username               = "admin"
+  password               = "veeranarni"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.lb_sg.id]
+  depends_on = [ aws_db_subnet_group.sub-grp ]
+  publicly_accessible = true
+  backup_retention_period = 7
+
+  
+  tags = {
+    DB_identifier = "book-rds"
+  }
+}
+
+resource "aws_db_subnet_group" "sub-grp" {
+  name       = "rds"
+  subnet_ids = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+
+  tags = {
+    Name = "My-DB-subnet-group"
+  }
+}
+
+
+output "rds_address" {
+  value = aws_db_instance.rds.address
+}
