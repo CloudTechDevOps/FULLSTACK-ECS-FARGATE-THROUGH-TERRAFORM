f9b02f046d1e4f827f441e3577fa0e474ccbf4cd
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-02-17 17:58:11 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -91,7 +91,7 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
